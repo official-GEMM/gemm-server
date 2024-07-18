c240b00860153a070ce3151c98946ea57b118d05
foura1210 <foura3207@gmail.com>
2024-07-19 04:18:48 +0900

    feat: 인증 예외 필터 및 핸들러 정의

---
+++ src/main/java/com/example/gemm_server/security/jwt/JwtAccessDeniedHandler.java
@@ -0,0 +1,23 @@
+package com.example.gemm_server.security.jwt;
+
+import com.example.gemm_server.common.util.ResponseUtil;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtAccessDeniedHandler implements AccessDeniedHandler {
+
+  @Override
+  public void handle(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AccessDeniedException accessDeniedException
+  ) throws IOException {
+    ResponseUtil.setJsonResponse(response, HttpServletResponse.SC_FORBIDDEN,
+        accessDeniedException.getMessage());
+  }
+}
---
+++ src/main/java/com/example/gemm_server/security/jwt/TokenExceptionFilter.java
@@ -0,0 +1,24 @@
+package com.example.gemm_server.security.jwt;
+
+import com.example.gemm_server.common.util.ResponseUtil;
+import com.example.gemm_server.exception.TokenException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class TokenExceptionFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    try {
+      filterChain.doFilter(request, response);
+    } catch (TokenException error) {
+      ResponseUtil.setJsonResponse(response, error.getStatusCode(), error.getMessage());
+    }
+  }
+}
