a9f1c3a63e864d802c37f5a1b0e0133cb2b21608
foura1210 <foura3207@gmail.com>
2024-07-19 04:19:10 +0900

    feat: jwt 설정

--- src/main/java/com/example/gemm_server/config/SecurityConfig.java
+++ src/main/java/com/example/gemm_server/config/SecurityConfig.java
@@ -1,5 +1,9 @@
 package com.example.gemm_server.config;
 
+import com.example.gemm_server.security.jwt.JwtAccessDeniedHandler;
+import com.example.gemm_server.security.jwt.JwtAuthenticationEntryPoint;
+import com.example.gemm_server.security.jwt.TokenAuthenticationFilter;
+import com.example.gemm_server.security.jwt.TokenExceptionFilter;
 import com.example.gemm_server.security.oauth2.CustomOAuth2UserService;
 import com.example.gemm_server.security.oauth2.OAuth2FailureHandler;
 import com.example.gemm_server.security.oauth2.OAuth2SuccessHandler;
@@ -11,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -22,6 +27,7 @@ public class SecurityConfig {
   private static final AntPathRequestMatcher path = new AntPathRequestMatcher("/**");
   private final OAuth2SuccessHandler oAuth2SuccessHandler;
   private final OAuth2FailureHandler oAuth2FailureHandler;
+  private final TokenAuthenticationFilter tokenAuthenticationFilter;
   private final CustomOAuth2UserService customOAuth2UserService;
 
   @Bean
@@ -48,6 +54,17 @@ public class SecurityConfig {
             .successHandler(oAuth2SuccessHandler)
             .failureHandler(oAuth2FailureHandler)
         )
+
+        // jwt 관련 설정
+        .addFilterBefore(tokenAuthenticationFilter,
+            UsernamePasswordAuthenticationFilter.class)
+        .addFilterBefore(new TokenExceptionFilter(),
+            tokenAuthenticationFilter.getClass())
+
+        // 인증 예외 핸들링
+        .exceptionHandling((exceptions) -> exceptions
+            .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+            .accessDeniedHandler(new JwtAccessDeniedHandler()))
     ;
 
     return http.build();
