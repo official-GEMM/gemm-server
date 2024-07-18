afbff5e2ad0736bb8e26a644bf54e8db0631b80f
foura1210 <foura3207@gmail.com>
2024-07-19 03:22:10 +0900

    feat: 멤버 엔티티 추가

--- src/main/java/com/example/gemm_server/GemmServerApplication.java
+++ src/main/java/com/example/gemm_server/GemmServerApplication.java
@@ -2,12 +2,14 @@ package com.example.gemm_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class GemmServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GemmServerApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(GemmServerApplication.class, args);
+  }
 
 }
