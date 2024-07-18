7928dee7718a5662241b67ff63b633a4a6e1e9ca
foura1210 <foura3207@gmail.com>
2024-07-19 04:16:32 +0900

    feat: oauth2 설정

---
+++ src/main/java/com/example/gemm_server/config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.gemm_server.config;
+
+import com.example.gemm_server.security.oauth2.CustomOAuth2UserService;
+import com.example.gemm_server.security.oauth2.OAuth2FailureHandler;
+import com.example.gemm_server.security.oauth2.OAuth2SuccessHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+  // TODO: 인증 인가 경로 설정
+  private static final AntPathRequestMatcher path = new AntPathRequestMatcher("/**");
+  private final OAuth2SuccessHandler oAuth2SuccessHandler;
+  private final OAuth2FailureHandler oAuth2FailureHandler;
+  private final CustomOAuth2UserService customOAuth2UserService;
+
+  @Bean
+  protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+        // rest api 설정
+        .csrf(AbstractHttpConfigurer::disable)
+        .cors(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .logout(AbstractHttpConfigurer::disable)
+        .sessionManagement(c ->
+            c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+        // request 인증, 인가 설정
+        .authorizeHttpRequests(request ->
+            request.requestMatchers(path).permitAll()
+                .anyRequest().authenticated()
+        )
+        .oauth2Login(oauth2 -> oauth2
+            .userInfoEndpoint(userInfo -> userInfo
+                .userService(customOAuth2UserService)
+            )
+            .successHandler(oAuth2SuccessHandler)
+            .failureHandler(oAuth2FailureHandler)
+        )
+    ;
+
+    return http.build();
+  }
+}
