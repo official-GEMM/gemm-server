caa29f1827a9c179149993e01b5b8935911a255e
foura1210 <foura3207@gmail.com>
2024-07-19 03:20:58 +0900

    feat: member에 필요한 enum 정의

---
+++ src/main/java/com/example/gemm_server/common/Provider.java
@@ -0,0 +1,22 @@
+package com.example.gemm_server.common;
+
+import lombok.Getter;
+
+@Getter
+public enum Provider {
+  KAKAO("kakao", "id"),
+  NAVER("naver", "id"),
+  GOOGLE("google", "sub");
+
+  private final String name;
+  private final String attributeNameOfId;
+
+  Provider(String name, String attributeNameOfId) {
+    this.name = name;
+    this.attributeNameOfId = attributeNameOfId;
+  }
+
+  public boolean isEqual(String name) {
+    return this.name.equals(name);
+  }
+}
