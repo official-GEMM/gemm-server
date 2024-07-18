d71fa7cd58aa83e76bc3d61bcea2d4bf21cef7b4
foura1210 <foura3207@gmail.com>
2024-07-19 04:14:42 +0900

    feat: oauth2 핸들러 정의

---
+++ src/main/java/com/example/gemm_server/security/oauth2/OAuth2SuccessHandler.java
@@ -0,0 +1,35 @@
+package com.example.gemm_server.security.oauth2;
+
+import com.example.gemm_server.security.jwt.TokenProvider;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RequiredArgsConstructor
+@Component
+public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
+
+  private final TokenProvider tokenProvider;
+  private static final String URI = "/auth/login/{userId}";
+
+  @Override
+  public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+      Authentication authentication) throws IOException, ServletException {
+    String accessToken = tokenProvider.generateAccessToken(authentication);
+    String refreshToken = tokenProvider.generateRefreshToken(authentication);
+    String userId = authentication.getName();
+    
+    String redirectUrl = UriComponentsBuilder.fromUriString(URI)
+        .queryParam("accessToken", accessToken)
+        .queryParam("refreshToken", refreshToken)
+        .buildAndExpand(userId, "userId").toUriString();
+
+    response.sendRedirect(redirectUrl);
+  }
+}
