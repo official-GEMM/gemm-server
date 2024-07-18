2d9fa149c020b4c94e64ca1f836ebded1f343be9
foura1210 <foura3207@gmail.com>
2024-07-19 04:06:33 +0900

    feat: 정책 상수 설정

---
+++ src/main/java/com/example/gemm_server/common/constant/Policy.java
@@ -0,0 +1,7 @@
+package com.example.gemm_server.common.constant;
+
+public class Policy {
+
+  public static final int ATTENDANCE_COMPENSATION = 30;
+  public static final int JOIN_COMPENSATION = 1000;
+}
