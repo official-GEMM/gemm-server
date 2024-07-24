1efa0d477b5bb1c09e2c7f580da4d0ac874c1de1
foura1210 <foura3207@gmail.com>
2024-07-24 17:58:27 +0900

    feat: 사용자 인증 및 권한 관리 annotation 수정

--- src/main/java/com/example/gemm_server/common/annotation/Admin.java
+++ src/main/java/com/example/gemm_server/common/annotation/Admin.java
@@ -1,9 +1,14 @@
 package com.example.gemm_server.common.annotation;
 
+import com.example.gemm_server.common.code.error.MemberErrorCode;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface Admin {
 
+  MemberErrorCode errorCode() default MemberErrorCode.LACK_OF_AUTHORITY;
 }
--- src/main/java/com/example/gemm_server/common/annotation/BearerAuth.java
+++ src/main/java/com/example/gemm_server/common/annotation/BearerAuth.java
@@ -1,9 +1,15 @@
 package com.example.gemm_server.common.annotation;
 
+import com.example.gemm_server.common.code.error.MemberErrorCode;
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(value = ElementType.METHOD)
 @Retention(value = RetentionPolicy.RUNTIME)
 public @interface BearerAuth {
+
+  MemberErrorCode errorCode() default MemberErrorCode.LOGIN_NECESSARY;
 
 }
