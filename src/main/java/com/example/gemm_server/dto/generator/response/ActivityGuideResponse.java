7c3436019149e4446961fe69c4bdf8696f169491
prefer52 <prefer0502@gmail.com>
2024-07-29 00:10:09 +0900

    feat: 응답 dto swagger 적용

--- src/main/java/com/example/gemm_server/dto/generator/response/ActivityGuideResponse.java
+++ src/main/java/com/example/gemm_server/dto/generator/response/ActivityGuideResponse.java
@@ -1,6 +1,9 @@
 package com.example.gemm_server.dto.generator.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record ActivityGuideResponse(
+        @Schema(description = "생성한 내용")
         String content
 ) {
 }
--- src/main/java/com/example/gemm_server/dto/generator/response/SavedActivityResponse.java
+++ src/main/java/com/example/gemm_server/dto/generator/response/SavedActivityResponse.java
@@ -1,6 +1,9 @@
 package com.example.gemm_server.dto.generator.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record SavedActivityResponse(
+        @Schema(description = "저장한 활동 방법 아이디")
         Long storageId
 ) {
 }
