f79256adf6804cd99dba02b217e809fbd4ed0cdb
foura1210 <foura3207@gmail.com>
2024-07-30 17:38:45 +0900

    refactor: gem 응답 클래스명 변경

---
+++ src/main/java/com/example/gemm_server/dto/common/response/GemResponse.java
@@ -0,0 +1,14 @@
+package com.example.gemm_server.dto.common.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+public class GemResponse {
+
+  @Schema(description = "보유 젬 개수")
+  private int gem;
+
+  public GemResponse() {
+  }
+}
