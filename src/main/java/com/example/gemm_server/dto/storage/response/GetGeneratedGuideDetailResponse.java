530c307b7caa699885894255799ea84ca83dc00e
foura1210 <foura3207@gmail.com>
2024-07-30 17:49:24 +0900

    rename: Response를 명시하여 변경된 클래스명 반영

--- src/main/java/com/example/gemm_server/dto/storage/response/GetGeneratedGuideDetailResponse.java
+++ src/main/java/com/example/gemm_server/dto/storage/response/GetGeneratedGuideDetailResponse.java
@@ -1,11 +1,11 @@
 package com.example.gemm_server.dto.storage.response;
 
-import com.example.gemm_server.dto.common.GuideDetail;
+import com.example.gemm_server.dto.common.response.GuideDetailResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
-public class GetGeneratedGuideDetailResponse extends GuideDetail {
+public class GetGeneratedGuideDetailResponse extends GuideDetailResponse {
 
   @Schema(description = "생성물 아이디")
   private long generationId;
--- src/main/java/com/example/gemm_server/dto/storage/response/GetGeneratedGuidesResponse.java
+++ src/main/java/com/example/gemm_server/dto/storage/response/GetGeneratedGuidesResponse.java
@@ -1,7 +1,7 @@
 package com.example.gemm_server.dto.storage.response;
 
-import com.example.gemm_server.dto.common.Guides;
-import com.example.gemm_server.dto.common.PageInformation;
+import com.example.gemm_server.dto.common.response.GuidesResponse;
+import com.example.gemm_server.dto.common.response.PageInformationResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
@@ -9,10 +9,10 @@ import lombok.Getter;
 public class GetGeneratedGuidesResponse {
 
   @Schema(description = "생성한 활동 방법 리스트")
-  private Guides[] guides;
+  private GuidesResponse[] guides;
 
   @Schema(description = "페이지 정보")
-  private PageInformation pageInfo;
+  private PageInformationResponse pageInfo;
 
   public GetGeneratedGuidesResponse() {
   }
