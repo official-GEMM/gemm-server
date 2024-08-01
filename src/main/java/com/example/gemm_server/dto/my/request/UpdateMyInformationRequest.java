94126ee004f354e76240426ba5d75bed89d833bd
foura1210 <foura3207@gmail.com>
2024-08-01 17:33:44 +0900

    feat: 내 정보 변경 api의 입력 검증

--- src/main/java/com/example/gemm_server/dto/my/request/UpdateMyInformationRequest.java
+++ src/main/java/com/example/gemm_server/dto/my/request/UpdateMyInformationRequest.java
@@ -2,6 +2,7 @@ package com.example.gemm_server.dto.my.request;
 
 import com.example.gemm_server.common.constant.RegularExpression;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -12,6 +13,7 @@ public class UpdateMyInformationRequest {
 
   @Schema(description = "관리 대상자의 연령")
   @NotNull
+  @Min(1)
   private Short manageAge;
 
   @Schema(description = "전화번호")
