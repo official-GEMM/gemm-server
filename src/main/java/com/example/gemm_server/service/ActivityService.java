cd77371e9765d8ee20a11c7a00a6746d7b0d4255
prefer52 <prefer0502@gmail.com>
2024-08-06 00:33:00 +0900

    feat: 자료 생성 API 구현

--- src/main/java/com/example/gemm_server/service/ActivityService.java
+++ src/main/java/com/example/gemm_server/service/ActivityService.java
@@ -2,6 +2,8 @@ package com.example.gemm_server.service;
 
 import com.example.gemm_server.common.constant.Policy;
 import com.example.gemm_server.common.enums.GemUsageType;
+import com.example.gemm_server.common.util.PoiUtil;
+import com.example.gemm_server.common.util.S3Util;
 import com.example.gemm_server.common.util.WebClientUtil;
 import com.example.gemm_server.domain.entity.Activity;
 import com.example.gemm_server.domain.entity.Generation;
@@ -9,12 +11,21 @@ import com.example.gemm_server.domain.entity.Member;
 import com.example.gemm_server.domain.repository.ActivityRepository;
 import com.example.gemm_server.domain.repository.GenerationRepository;
 import com.example.gemm_server.dto.generator.request.GenerateGuideRequest;
+import com.example.gemm_server.dto.generator.request.GenerateMaterialRequest;
 import com.example.gemm_server.dto.generator.request.SaveGuideRequest;
 import com.example.gemm_server.dto.generator.request.UpdateGuideRequest;
+import com.example.gemm_server.dto.generator.response.ActivitySheetPathResponse;
+import com.example.gemm_server.dto.generator.response.CutoutPathResponse;
 import com.example.gemm_server.dto.generator.response.GeneratedGuideResponse;
+import com.example.gemm_server.dto.generator.response.GeneratedMaterialsResponse;
 import com.example.gemm_server.dto.generator.response.LlmGuideResponse;
+import com.example.gemm_server.dto.generator.response.LlmMaterialResponse;
+import com.example.gemm_server.dto.generator.response.PptPathResponse;
 import com.example.gemm_server.dto.generator.response.SavedGenerationResponse;
 import com.example.gemm_server.dto.generator.response.UpdatedGuideResponse;
+import jakarta.transaction.Transactional;
+import java.io.File;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -30,7 +41,9 @@ public class ActivityService {
   private final GenerationRepository generationRepository;
   private final GemService gemService;
   private final MemberService memberService;
+  private final S3Util s3Util;
 
+  @Transactional
   public GeneratedGuideResponse generateGuide(GenerateGuideRequest generateGuideRequest,
       Long memberId) {
     Member member = memberService.findMemberByMemberId(memberId);
@@ -43,24 +56,21 @@ public class ActivityService {
     return new GeneratedGuideResponse(llmGuideResponse.content(), member.getGem());
   }
 
+  @Transactional
   public SavedGenerationResponse saveGuide(SaveGuideRequest saveGuideRequest, Long memberId) {
     Member member = memberService.findMemberByMemberId(memberId);
 
     Activity activity = Activity.builder().title(saveGuideRequest.title())
-        .age(saveGuideRequest.age())
-        .content(saveGuideRequest.content())
-        .materialType((short) 0)
+        .age(saveGuideRequest.age()).content(saveGuideRequest.content()).materialType((short) 0)
         .build();
     Activity savedActivity = activityRepository.save(activity);
 
-    Generation generation = Generation.builder()
-        .activity(savedActivity)
-        .owner(member)
-        .build();
+    Generation generation = Generation.builder().activity(savedActivity).owner(member).build();
     Generation savedGeneration = generationRepository.save(generation);
     return new SavedGenerationResponse(savedGeneration.getId());
   }
 
+  @Transactional
   public UpdatedGuideResponse updateGuide(UpdateGuideRequest UpdateGuideRequest, Long memberId) {
     Member member = memberService.findMemberByMemberId(memberId);
 
@@ -72,5 +82,41 @@ public class ActivityService {
     return new UpdatedGuideResponse(updatedGuideResponse.content(), member.getGem());
   }
 
+  @Transactional
+  public GeneratedMaterialsResponse generateMaterials(
+      GenerateMaterialRequest generateMaterialRequest, Long memberId) {
+    Member member = memberService.findMemberByMemberId(memberId);
 
+    LlmMaterialResponse llmMaterialResponse = webClientUtil.post("/generate/activity/materials",
+        generateMaterialRequest, LlmMaterialResponse.class);
+
+    PptPathResponse pptPathResponse = null;
+    if (llmMaterialResponse.ppt() != null) {
+      List<String> imagePaths = PoiUtil.pptToImages(
+          s3Util.downloadFile(llmMaterialResponse.ppt().fileName()));
+      String[] thumbnailPaths = new String[imagePaths.size()];
+      for (int i = 0; i < imagePaths.size(); i++) {
+        File file = new File(imagePaths.get(i));
+        thumbnailPaths[i] = s3Util.getFileUrl(s3Util.uploadFile(file));
+      }
+      pptPathResponse = new PptPathResponse(thumbnailPaths, llmMaterialResponse.ppt().filePath());
+      gemService.saveChangesOfGemWithMember(member, Policy.GENERATE_PPT, GemUsageType.AI_USE);
+    }
+
+    ActivitySheetPathResponse activitySheetPathResponse = null;
+    if (llmMaterialResponse.activitySheet() != null) {
+      gemService.saveChangesOfGemWithMember(member, Policy.GENERATE_ACTIVITY_SHEET,
+          GemUsageType.AI_USE);
+    }
+
+    CutoutPathResponse cutoutPathResponse = null;
+    if (llmMaterialResponse.cutout() != null) {
+      cutoutPathResponse = new CutoutPathResponse(llmMaterialResponse.cutout(),
+          llmMaterialResponse.cutout());
+      gemService.saveChangesOfGemWithMember(member, Policy.GENERATE_CUTOUT, GemUsageType.AI_USE);
+    }
+
+    return new GeneratedMaterialsResponse(pptPathResponse, activitySheetPathResponse,
+        cutoutPathResponse, member.getGem());
+  }
 }
