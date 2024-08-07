285f33d3a427a0b1c25fe9fb1fdc6c49ad98b375
foura1210 <foura3207@gmail.com>
2024-08-08 02:36:33 +0900

    feat: 알림 도메인 설정

---
+++ src/main/java/com/example/gemm_server/domain/repository/NotificationRepository.java
@@ -0,0 +1,22 @@
+package com.example.gemm_server.domain.repository;
+
+import com.example.gemm_server.domain.entity.Notification;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+
+  @Query("SELECT e FROM Notification e WHERE e.createdAt >= :dateThreshold AND e.receiver.id = :memberId")
+  List<Notification> findByReceiverIdAndCreatedAtDateAfter(
+      @Param("memberId") Long memberId,
+      @Param("dateThreshold") LocalDateTime dateThreshold);
+
+  @Query("SELECT COUNT(e) FROM Notification e WHERE e.createdAt >= :dateThreshold AND e.isOpened = :isOpened AND e.receiver.id = :memberId")
+  Integer countByReceiverIdAndOpenedAndCreatedAtDateAfter(
+      @Param("memberId") Long memberId,
+      @Param("isOpened") boolean isOpened,
+      @Param("dateThreshold") LocalDateTime dateThreshold);
+}
