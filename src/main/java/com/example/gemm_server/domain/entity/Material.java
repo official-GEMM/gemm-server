94ce49226a984363ea11645d90ee003b3e65d019
foura1210 <foura3207@gmail.com>
2024-08-11 17:53:26 +0900

    feat: Material에 타입 및 썸네일과의 매핑 추가

--- src/main/java/com/example/gemm_server/domain/entity/Material.java
+++ src/main/java/com/example/gemm_server/domain/entity/Material.java
@@ -1,14 +1,19 @@
 package com.example.gemm_server.domain.entity;
 
+import com.example.gemm_server.common.enums.MaterialType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -40,9 +45,16 @@ public class Material extends Timestamped {
   @Column(name = "file_path", nullable = false)
   private String filePath;
 
+  @Enumerated(value = EnumType.STRING)
+  @Column(name = "type", nullable = false)
+  private MaterialType type;
+
   @ManyToOne(targetEntity = Activity.class, fetch = FetchType.LAZY)
   @JoinColumn(name = "activity_id", nullable = false)
   private Activity activity;
+
+  @OneToMany(mappedBy = "material")
+  private List<Thumbnail> thumbnails;
 
   @Builder
   public Material(String originName, String fileName, String filePath, Activity activity) {
