1c2aa5e8d95e4c17b7a0e167fc1e64fc66d21096
foura1210 <foura3207@gmail.com>
2024-08-11 17:51:41 +0900

    feat: activity에 category 추가

--- src/main/java/com/example/gemm_server/domain/entity/Activity.java
+++ src/main/java/com/example/gemm_server/domain/entity/Activity.java
@@ -1,7 +1,14 @@
 package com.example.gemm_server.domain.entity;
 
-import com.example.gemm_server.common.enums.MaterialType;
-import jakarta.persistence.*;
+import com.example.gemm_server.common.enums.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,30 +22,35 @@ import org.hibernate.annotations.SQLRestriction;
 @SQLRestriction("deleted_at is NULL")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "activity")
-public class Activity extends Timestamped{
+public class Activity extends Timestamped {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
 
-    @Column(name="title", length = 30, nullable = false)
-    private String title;
+  @Column(name = "title", length = 30, nullable = false)
+  private String title;
 
-    @Column(name="age", nullable = false)
-    private Short age;
+  @Column(name = "age", nullable = false)
+  private Short age;
 
-    @Column(name="material_type", columnDefinition = "BIT(3)", nullable = false)
-    private Short materialType;
+  @Enumerated(value = EnumType.STRING)
+  @Column(name = "category", nullable = false)
+  private Category category;
 
-    @Column(name="content", columnDefinition = "TEXT", nullable = false)
-    private String content;
+  @Column(name = "material_type", columnDefinition = "BIT(3)", nullable = false)
+  private Short materialType;
 
-    @Builder
-    public Activity(String title, Short age, Short materialType, String content) {
-        this.title = title;
-        this.age = age;
-        this.materialType = materialType;
-        this.content = content;
-    }
+  @Column(name = "content", columnDefinition = "TEXT", nullable = false)
+  private String content;
+
+  @Builder
+  public Activity(String title, Short age, Short materialType, Category category, String content) {
+    this.title = title;
+    this.age = age;
+    this.materialType = materialType;
+    this.category = category;
+    this.content = content;
+  }
 }
