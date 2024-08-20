62b5818bb26bfd0939a08ff72e2c54663f4262cd
foura1210 <foura3207@gmail.com>
2024-08-20 13:02:33 +0900

    feat: allowed origin 등록

---
+++ src/main/java/com/example/gemm_server/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.gemm_server.config;
+
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsConfig implements WebMvcConfigurer {
+
+  private String[] allowedOrigins;
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+
+    registry.addMapping("/**")
+        .allowedOrigins(allowedOrigins)
+        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+        .allowCredentials(true)
+        .maxAge(3000);
+  }
+}
