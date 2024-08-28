b316b9d3d26a55e00fff3f1cc4ada654304ec00a
prefer52 <prefer0502@gmail.com>
2024-08-28 23:37:44 +0900

    fix: 활동 방법 dto 수정

--- src/main/java/com/example/gemm_server/dto/generator/response/GenerateGuideResponse.java
+++ src/main/java/com/example/gemm_server/dto/generator/response/GenerateGuideResponse.java
@@ -2,10 +2,10 @@ package com.example.gemm_server.dto.generator.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "생성한 활동 방법 응답", requiredProperties = {"content", "gem"})
+@Schema(description = "생성한 활동 방법 응답", requiredProperties = {"contents", "gem"})
 public record GenerateGuideResponse(
     @Schema(description = "생성한 내용")
-    String content,
+    ContentResponse[] contents,
     @Schema(description = "잔여 젬")
     int gem
 ) {
--- src/main/java/com/example/gemm_server/dto/generator/response/UpdatedGuideResponse.java
+++ src/main/java/com/example/gemm_server/dto/generator/response/UpdatedGuideResponse.java
@@ -2,10 +2,10 @@ package com.example.gemm_server.dto.generator.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "수정된 활동 방법 응답", requiredProperties = {"content", "gem"})
+@Schema(description = "수정된 활동 방법 응답", requiredProperties = {"contents", "gem"})
 public record UpdatedGuideResponse(
     @Schema(description = "수정하여 생성한 내용")
-    String content,
+    ContentResponse[] contents,
     @Schema(description = "잔여 젬")
     int gem
 ) {
