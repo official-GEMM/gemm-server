a49cc4d4dc7ef69e4761380820e0dcd0c149a8e4
foura1210 <foura3207@gmail.com>
2024-09-02 21:12:47 +0900

    GM-41 feat: content dto 생성

---
+++ src/main/java/com/example/gemm_server/dto/common/response/ContentResponse.java
@@ -0,0 +1,21 @@
+package com.example.gemm_server.dto.common.response;
+
+import com.example.gemm_server.common.enums.Format;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "활동 응답", requiredProperties = {"content", "format"})
+public class ContentResponse {
+
+  @Schema(description = "내용")
+  private final String content;
+
+  @Schema(description = "썸네일 경로")
+  private final Format format;
+
+  public ContentResponse(String content) {
+    this.format = Format.getFormat(content);
+    this.content = this.format.removePrefix(content);
+  }
+}
