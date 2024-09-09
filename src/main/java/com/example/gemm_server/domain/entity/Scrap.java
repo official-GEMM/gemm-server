752141d11cfbd573c98f070b03f08d7212c1654c
foura1210 <foura3207@gmail.com>
2024-09-09 19:35:19 +0900

    GM-60 feat: scrap entity 정의

---
+++ src/main/java/com/example/gemm_server/domain/entity/Scrap.java
@@ -0,0 +1,42 @@
+package com.example.gemm_server.domain.entity;
+
+import com.example.gemm_server.common.annotation.entity.ColumnDescription;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Getter
+@Entity
+@SQLDelete(sql = "UPDATE scrap SET deleted_at = CURRENT_TIMESTAMP where id = ?")
+@SQLRestriction("deleted_at is NULL")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "scrap")
+public class Scrap extends Timestamped {
+
+  @Id
+  @ColumnDescription("아이디")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "id")
+  private Long id;
+
+  @ColumnDescription("스크랩한 사용자 아이디")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", nullable = false)
+  private Member member;
+
+  @ColumnDescription("마켓 아이템 아이디")
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "market_item_id", nullable = false)
+  private MarketItem marketItem;
+}
