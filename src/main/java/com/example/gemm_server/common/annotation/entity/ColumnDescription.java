e020959500594fb578cb8bfdf4305bf8ce0117c5
foura1210 <foura3207@gmail.com>
2024-09-09 19:31:50 +0900

    GM-60 feat: entity의 각 column에 대한 설명 annotation 정의

---
+++ src/main/java/com/example/gemm_server/common/annotation/entity/ColumnDescription.java
@@ -0,0 +1,13 @@
+package com.example.gemm_server.common.annotation.entity;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface ColumnDescription {
+
+  String value();
+}
