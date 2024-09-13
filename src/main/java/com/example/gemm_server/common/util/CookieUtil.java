28ea1cb5c9ae8b8e053eb354802788edaed73b0c
foura1210 <foura3207@gmail.com>
2024-09-14 02:07:55 +0900

    GM-70 feat: 로그인 응답 시 refreshToken cookie 추가

--- src/main/java/com/example/gemm_server/common/util/CookieUtil.java
+++ src/main/java/com/example/gemm_server/common/util/CookieUtil.java
@@ -1,19 +1,18 @@
 package com.example.gemm_server.common.util;
 
+import java.time.Duration;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CookieUtil {
 
-  private static final int refreshTokenCookieExpiration = 7 * 24 * 60 * 60;  // 7일
-
   public static ResponseCookie createForRefreshToken(String refreshToken) {
     return ResponseCookie.from("refreshToken", refreshToken)
         .httpOnly(true)
         .secure(true)
         .path("/")
-        .maxAge(refreshTokenCookieExpiration)
+        .maxAge(Duration.ofDays(7))
         .sameSite("None")
         .build();
   }
