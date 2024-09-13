65a95fd468bbd1cd6d043f1e76402dd391aa67c7
foura1210 <foura3207@gmail.com>
2024-09-13 15:52:20 +0900

    GM-72 fix: gem 변경에 대한 예외 처리 추가

--- src/main/java/com/example/gemm_server/common/code/error/GemErrorCode.java
+++ src/main/java/com/example/gemm_server/common/code/error/GemErrorCode.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum GemErrorCode implements ErrorCode {
 
+  GEM_TO_BE_POSITIVE(HttpStatus.BAD_REQUEST, "젬의 양은 양수로 전달되어야 합니다."),
   NOT_ENOUGH_GEM(HttpStatus.BAD_REQUEST, "젬이 부족합니다."),
 
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 에러입니다. 서버 팀에 연락주세요!");
