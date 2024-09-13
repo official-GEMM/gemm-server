0eebb46cf34953359cc2c05fd599d626dca224ed
prefer52 <prefer0502@gmail.com>
2024-09-13 20:52:18 +0900

    GM-73 feat: 활동 방법 저장 수정

--- src/main/java/com/example/gemm_server/dto/generator/request/SaveGuideRequest.java
+++ src/main/java/com/example/gemm_server/dto/generator/request/SaveGuideRequest.java
@@ -2,9 +2,12 @@ package com.example.gemm_server.dto.generator.request;
 
 import com.example.gemm_server.common.enums.Category;
 import com.example.gemm_server.domain.entity.Activity;
+import com.example.gemm_server.dto.common.response.ContentResponse;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import java.util.List;
 
 public record SaveGuideRequest(
     @Schema(description = "생성하고자 하는 주제")
@@ -20,8 +23,9 @@ public record SaveGuideRequest(
     Category category,
 
     @Schema(description = "생성 내용")
-    @NotBlank
-    String content
+    @NotNull
+    @Size(min = 1)
+    List<ContentResponse> contents
 ) {
 
   public Activity toEntity() {
@@ -29,7 +33,7 @@ public record SaveGuideRequest(
         .title(title())
         .age(age())
         .category(category())
-        .content(content())
+        .content(ContentResponse.getCompleteFormatString(contents))
         .materialType((short) 0)
         .build();
   }
