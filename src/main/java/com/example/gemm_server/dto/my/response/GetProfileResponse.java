7262422a5b0313e5004385d5f162b6f14841d213
foura1210 <foura3207@gmail.com>
2024-09-24 16:34:04 +0900

    GM-93 feat: 내 프로필 조회 api의 응답 수정

--- src/main/java/com/example/gemm_server/dto/my/response/GetProfileResponse.java
+++ src/main/java/com/example/gemm_server/dto/my/response/GetProfileResponse.java
@@ -5,7 +5,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
-@Schema(description = "내 프로필 응답", requiredProperties = {"profileImagePath", "referralCode"})
+@Schema(description = "내 프로필 응답", requiredProperties = {"profileImagePath", "referralCode",
+    "nickname"})
 public class GetProfileResponse {
 
   @Schema(description = "프로필 이미지")
@@ -14,8 +15,13 @@ public class GetProfileResponse {
   @Schema(description = "사용자 추천인 코드")
   private String referralCode;
 
+  @Schema(description = "시용자 닉네임")
+  private String nickname;
+
+
   public GetProfileResponse(MemberBundle memberBundle) {
     this.profileImagePath = memberBundle.getProfileImageUrl();
     this.referralCode = memberBundle.getMember().getReferralCode();
+    this.nickname = memberBundle.getMember().getNickname();
   }
 }
