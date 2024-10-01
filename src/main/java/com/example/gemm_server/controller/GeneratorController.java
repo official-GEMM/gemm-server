f1b7a439af7df12ea3593de76e7765d9fd2c8039
foura1210 <foura3207@gmail.com>
2024-10-01 22:45:51 +0900

    GM-129 feat: generator에 로그인 기능 연동하기

--- src/main/java/com/example/gemm_server/controller/GeneratorController.java
+++ src/main/java/com/example/gemm_server/controller/GeneratorController.java
@@ -1,5 +1,6 @@
 package com.example.gemm_server.controller;
 
+import com.example.gemm_server.common.annotation.auth.BearerAuth;
 import com.example.gemm_server.dto.CommonResponse;
 import com.example.gemm_server.dto.generator.request.GenerateGuideRequest;
 import com.example.gemm_server.dto.generator.request.GenerateMaterialRequest;
@@ -19,19 +20,21 @@ import com.example.gemm_server.dto.generator.response.UpdatedActivitySheetRespon
 import com.example.gemm_server.dto.generator.response.UpdatedCutoutResponse;
 import com.example.gemm_server.dto.generator.response.UpdatedGuideResponse;
 import com.example.gemm_server.dto.generator.response.UpdatedPptResponse;
+import com.example.gemm_server.security.jwt.CustomUser;
 import com.example.gemm_server.service.ActivityService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@BearerAuth
+@BearerAuth
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/generate")
@@ -43,32 +46,33 @@ public class GeneratorController {
   @Operation(summary = "활동 방법 생성", description = "활동 방법을 생성하는 API")
   @PostMapping("/guide")
   public ResponseEntity<CommonResponse<GenerateGuideResponse>> generateActivityGuide(
-      @Valid @RequestBody GenerateGuideRequest generateGuideRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody GenerateGuideRequest generateGuideRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     GenerateGuideResponse generateGuideResponse = activityService.generateGuide(
-        generateGuideRequest, 1L);
+        generateGuideRequest, user.getId());
     return ResponseEntity.ok(new CommonResponse<>(generateGuideResponse));
   }
 
   @Operation(summary = "활동 방법 저장", description = "활동 방법을 내 저장소에 저장하는 API")
   @PostMapping("/guide/result")
   public ResponseEntity<CommonResponse<SavedGuideResponse>> saveActivityGuide(
-      @Valid @RequestBody SaveGuideRequest saveGuideRequest
-//            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody SaveGuideRequest saveGuideRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     SavedGuideResponse savedGuideResponse = activityService.saveGuide(saveGuideRequest,
-        1L);
+        user.getId());
     return ResponseEntity.ok(new CommonResponse<>(savedGuideResponse));
   }
 
   @Operation(summary = "활동 방법 수정", description = "활동 방법을 수정하는 API")
   @PutMapping("/guide/result")
   public ResponseEntity<CommonResponse<UpdatedGuideResponse>> updateActivityGuide(
-      @Valid @RequestBody UpdateGuideRequest updateGuideRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody UpdateGuideRequest updateGuideRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
-    UpdatedGuideResponse updatedGuideResponse = activityService.updateGuide(updateGuideRequest, 1L);
+    UpdatedGuideResponse updatedGuideResponse = activityService.updateGuide(updateGuideRequest,
+        user.getId());
     return ResponseEntity.ok(new CommonResponse<>(updatedGuideResponse));
   }
 
@@ -76,7 +80,6 @@ public class GeneratorController {
   @PostMapping("/guide/sync")
   public ResponseEntity<CommonResponse<LinkedMaterialGuideResponse>> linkGuideToMaterial(
       @Valid @RequestBody LinkMaterialGuideRequest linkMaterialGuideRequest
-      //            @AuthenticationPrincipal CustomUser user
   ) {
     LinkedMaterialGuideResponse linkedMaterialGuideResponse = activityService.linkGuideToMaterial(
         linkMaterialGuideRequest);
@@ -86,54 +89,55 @@ public class GeneratorController {
   @Operation(summary = "활동 자료 생성", description = "활동 자료를 생성하는 API")
   @PostMapping("/materials")
   public ResponseEntity<CommonResponse<GeneratedMaterialsResponse>> generateMaterial(
-      @Valid @RequestBody GenerateMaterialRequest generateMaterialRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody GenerateMaterialRequest generateMaterialRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     GeneratedMaterialsResponse generatedMaterialsResponse = activityService.generateMaterials(
-        generateMaterialRequest, 1L);
+        generateMaterialRequest, user.getId());
     return ResponseEntity.ok(new CommonResponse<>(generatedMaterialsResponse));
   }
 
   @Operation(summary = "활동 자료 저장", description = "활동 자료를 저장하는 API")
   @PostMapping("/materials/result")
   public ResponseEntity<CommonResponse<SavedMaterialResponse>> saveMaterial(
-      @Valid @RequestBody SaveMaterialRequest saveMaterialRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody SaveMaterialRequest saveMaterialRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     SavedMaterialResponse savedMaterialResponse = activityService.saveMaterials(saveMaterialRequest,
-        1L);
+        user.getId());
     return ResponseEntity.ok(new CommonResponse<>(savedMaterialResponse));
   }
 
   @Operation(summary = "PPT 자료 수정", description = "PPT를 수정하는 API")
   @PutMapping("/materials/result/ppt")
   public ResponseEntity<CommonResponse<UpdatedPptResponse>> updatePpt(
-      @Valid @RequestBody UpdatePptRequest updatePptRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody UpdatePptRequest updatePptRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
-    UpdatedPptResponse updatedPptResponse = activityService.updatePpt(updatePptRequest, 1L);
+    UpdatedPptResponse updatedPptResponse = activityService.updatePpt(updatePptRequest,
+        user.getId());
     return ResponseEntity.ok(new CommonResponse<>(updatedPptResponse));
   }
 
   @Operation(summary = "활동지 자료 수정", description = "활동지를 수정하는 API")
   @PutMapping("/materials/result/activitySheet")
   public ResponseEntity<CommonResponse<UpdatedActivitySheetResponse>> updateActivitySheet(
-      @Valid @RequestBody UpdateActivitySheetRequest updateActivitySheetRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody UpdateActivitySheetRequest updateActivitySheetRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     UpdatedActivitySheetResponse updatedActivitySheetResponse = activityService.updateActivitySheet(
-        updateActivitySheetRequest, 1L);
+        updateActivitySheetRequest, user.getId());
     return ResponseEntity.ok(new CommonResponse<>(updatedActivitySheetResponse));
   }
 
   @Operation(summary = "컷/도안 자료 수정", description = "컷/도안을 수정하는 API")
   @PutMapping("/materials/result/cutout")
   public ResponseEntity<CommonResponse<UpdatedCutoutResponse>> updateCutout(
-      @Valid @RequestBody UpdateCutoutRequest updateCutoutRequest
-      //            @AuthenticationPrincipal CustomUser user
+      @Valid @RequestBody UpdateCutoutRequest updateCutoutRequest,
+      @AuthenticationPrincipal CustomUser user
   ) {
     UpdatedCutoutResponse updatedCutoutResponse = activityService.updateCutout(updateCutoutRequest,
-        1L);
+        user.getId());
     return ResponseEntity.ok(new CommonResponse<>(updatedCutoutResponse));
   }
 }
