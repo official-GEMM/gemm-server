bb098207ad0bfd25dab4c5fafae0857539f4da9f
foura1210 <foura3207@gmail.com>
2024-10-02 20:05:39 +0900

    GM-131 fix: localDate에 pattern 적용 취소

--- src/main/java/com/example/gemm_server/common/constant/RegularExpression.java
+++ src/main/java/com/example/gemm_server/common/constant/RegularExpression.java
@@ -5,5 +5,4 @@ public class RegularExpression {
   public static final String NAME = "^[가-힣a-zA-Z]{2,30}$";
   public static final String NICKNAME = "^[가-힣]{2,12}$";
   public static final String PHONE_NUMBER = "^01[016789]-\\d{3,4}-\\d{4}$";
-  public static final String DATE = "^\\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12]\\d|3[01])$";
 }
