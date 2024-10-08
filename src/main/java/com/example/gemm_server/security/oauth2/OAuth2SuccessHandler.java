38decb77a72eabb80058dc3e5a40b82ffac38958
foura1210 <foura3207@gmail.com>
2024-10-08 20:53:53 +0900

    GM-140 feat: 로그인 후 리다이렉트 시 회원가입 보상 지급 여부 전달

--- src/main/java/com/example/gemm_server/security/oauth2/OAuth2SuccessHandler.java
+++ src/main/java/com/example/gemm_server/security/oauth2/OAuth2SuccessHandler.java
@@ -2,7 +2,6 @@ package com.example.gemm_server.security.oauth2;
 
 import com.example.gemm_server.common.util.CookieUtil;
 import com.example.gemm_server.security.jwt.TokenProvider;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -20,20 +19,24 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class OAuth2SuccessHandler implements AuthenticationSuccessHandler {
 
   private final TokenProvider tokenProvider;
-  private static final String URI = "/auth/login";
 
   @Value("${authentication.domain.cookie.refresh-token}")
   private String refreshTokenCookieDomain;
+  @Value("${authentication.url.login-redirect}")
+  private String loginRedirectUrl;
 
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-      Authentication authentication) throws IOException, ServletException {
+      Authentication authentication) throws IOException {
     String refreshToken = tokenProvider.generateRefreshTokenAndSave(authentication);
     ResponseCookie refreshTokenCookie = CookieUtil.createForRefreshToken(refreshToken,
         refreshTokenCookieDomain);
-
     response.addHeader(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString());
-    String redirectUrl = UriComponentsBuilder.fromUriString(URI).toUriString();
-    response.sendRedirect(redirectUrl);
+
+    CustomOauth2User user = (CustomOauth2User) authentication.getPrincipal();
+    String redirectWithParams = UriComponentsBuilder.fromUriString(loginRedirectUrl)
+        .queryParam("isJoinCompensationReceived", user.isJoinCompensationReceived())
+        .toUriString();
+    response.sendRedirect(redirectWithParams);
   }
 }
