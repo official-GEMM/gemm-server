b1ca1a0d674c35cf9d8ff4393b795789f587191f
foura1210 <foura3207@gmail.com>
2024-11-03 18:38:06 +0900

    GM-80 feat: 검색 조건에 따른 specification 조회 기능 구현

--- src/main/java/com/example/gemm_server/common/enums/SearchType.java
+++ src/main/java/com/example/gemm_server/common/enums/SearchType.java
@@ -1,14 +1,31 @@
 package com.example.gemm_server.common.enums;
 
+import com.example.gemm_server.domain.entity.MarketItem;
+import com.example.gemm_server.service.MarketItemSpecification;
 import lombok.Getter;
+import org.springframework.data.jpa.domain.Specification;
 
 @Getter
 public enum SearchType {
-  TOTAL("전체"),
-  TITLE("제목"),
-  SELLER("판매자");
+  TOTAL("전체") {
+    public Specification<MarketItem> getSpecification(String word) {
+      return MarketItemSpecification.hasOwner(word).or(MarketItemSpecification.hasTitle(word));
+    }
+  },
+  TITLE("제목") {
+    public Specification<MarketItem> getSpecification(String word) {
+      return MarketItemSpecification.hasTitle(word);
+    }
+  },
+  SELLER("판매자") {
+    public Specification<MarketItem> getSpecification(String word) {
+      return MarketItemSpecification.hasOwner(word);
+    }
+  };
 
   private final String description;
+
+  public abstract Specification<MarketItem> getSpecification(String word);
 
   SearchType(String description) {
     this.description = description;
