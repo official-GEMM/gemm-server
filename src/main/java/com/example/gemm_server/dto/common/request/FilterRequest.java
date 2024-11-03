ea250a2890cfb8c0e36c5324e5b4384477e55c8b
foura1210 <foura3207@gmail.com>
2024-11-03 18:34:55 +0900

    GM-80 feat: 필터 조건 일부를 리스트로 변경

--- src/main/java/com/example/gemm_server/dto/common/request/FilterRequest.java
+++ src/main/java/com/example/gemm_server/dto/common/request/FilterRequest.java
@@ -5,6 +5,7 @@ import com.example.gemm_server.common.enums.MaterialType;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,10 +14,10 @@ import lombok.Setter;
 public class FilterRequest {
 
   @Schema(description = "연령")
-  private Short age;
+  private List<Short> ages;
 
   @Schema(description = "영역 및 활동")
-  private Category category;
+  private List<Category> categories;
 
   @Schema(description = "연도")
   private Integer year;
@@ -27,7 +28,7 @@ public class FilterRequest {
   private Short month;
 
   @Schema(description = "자료 종류")
-  private MaterialType[] materialType;
+  private List<MaterialType> materialTypes;
 
   @Schema(description = "무료 여부")
   private Boolean isFree;
