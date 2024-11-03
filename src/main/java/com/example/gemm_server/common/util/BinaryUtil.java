8c165068c68625bc9612a3706331490ba567c89c
foura1210 <foura3207@gmail.com>
2024-11-03 18:30:54 +0900

    GM-80 refactor: materialUtil 리팩토링

--- src/main/java/com/example/gemm_server/common/util/BinaryUtil.java
+++
@@ -1,9 +0,0 @@
-package com.example.gemm_server.common.util;
-
-public class BinaryUtil {
-
-  public static String parseBinaryString(Short value) {
-    String binaryString = Integer.toBinaryString(Short.toUnsignedInt(value));
-    return String.format("%3s", binaryString).replace(' ', '0');
-  }
-}
