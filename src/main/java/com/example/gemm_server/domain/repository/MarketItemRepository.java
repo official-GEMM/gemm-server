c3e2d89ab5c4c7cb79c8e5b88bcfa7ae7b3d5b12
foura1210 <foura3207@gmail.com>
2024-11-08 15:44:18 +0900

    GM-174 feat: 마켓 레포지토리 수정

--- src/main/java/com/example/gemm_server/domain/repository/MarketItemRepository.java
+++ src/main/java/com/example/gemm_server/domain/repository/MarketItemRepository.java
@@ -2,6 +2,7 @@ package com.example.gemm_server.domain.repository;
 
 import com.example.gemm_server.domain.entity.MarketItem;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
@@ -9,6 +10,12 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MarketItemRepository extends JpaRepository<MarketItem, Long> {
+
+  @EntityGraph(attributePaths = {"activity"})
+  Optional<MarketItem> findWithActivityById(Long marketItemId);
+
+  @EntityGraph(attributePaths = {"activity", "owner"})
+  Optional<MarketItem> findWithActivityAndOwnerById(Long marketItemId);
 
   @EntityGraph(attributePaths = {"activity", "owner"})
   Page<MarketItem> findWithActivityAndOwnerBy(Pageable pageable);
@@ -19,4 +26,5 @@ public interface MarketItemRepository extends JpaRepository<MarketItem, Long> {
   @EntityGraph(attributePaths = {"activity", "owner"})
   List<MarketItem> findAll(Specification<MarketItem> spec);
 
+  MarketItem findByActivityId(Long activityId);
 }
