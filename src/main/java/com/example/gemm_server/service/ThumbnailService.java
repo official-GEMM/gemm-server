556fd7de73e5c46d68143ef06aca5f40f11a79bf
foura1210 <foura3207@gmail.com>
2024-11-10 16:29:41 +0900

    GM-181 feat: 썸네일 생성 및 저장하는 서비스 구현

--- src/main/java/com/example/gemm_server/service/ThumbnailService.java
+++ src/main/java/com/example/gemm_server/service/ThumbnailService.java
@@ -1,10 +1,18 @@
 package com.example.gemm_server.service;
 
+import com.example.gemm_server.common.constant.FilePath;
+import com.example.gemm_server.common.util.PoiUtil;
+import com.example.gemm_server.common.util.S3Util;
+import com.example.gemm_server.domain.entity.Material;
 import com.example.gemm_server.domain.entity.Thumbnail;
 import com.example.gemm_server.domain.repository.ThumbnailRepository;
+import com.example.gemm_server.dto.common.TypedMaterialFile;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
@@ -21,5 +29,29 @@ public class ThumbnailService {
     Optional<Thumbnail> thumbnail =
         thumbnailRepository.findFirstByMaterialActivityIdOrderByIdDesc(activityId);
     return thumbnail.orElse(null);
+  }
+
+  public List<Thumbnail> generateThumbnailAndSaveToS3AndDB(Material material,
+      TypedMaterialFile materialFile) {
+    List<MultipartFile> thumbnailFiles = PoiUtil.convertFileToPngs(materialFile,
+        material.getFileName());
+    return IntStream.range(0, thumbnailFiles.size())
+        .mapToObj(index -> saveToS3AndDB(material, thumbnailFiles.get(index), index)).toList();
+  }
+
+  public Thumbnail saveToS3AndDB(Material material, MultipartFile thumbnailFile,
+      int sequence) {
+    String saveDirectoryPath = FilePath.getThumbnailFileSaveDirectoryPath(material.getType());
+    String fileName = thumbnailFile.getOriginalFilename();
+    S3Util.uploadFile(thumbnailFile, fileName, saveDirectoryPath);
+
+    Thumbnail thumbnail = Thumbnail.builder()
+        .originName(fileName)
+        .fileName(fileName)
+        .directoryPath(saveDirectoryPath)
+        .sequence((short) sequence)
+        .material(material)
+        .build();
+    return thumbnailRepository.save(thumbnail);
   }
 }
