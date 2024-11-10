3977971d6921724edf72ded426a0ba0257ef6cdb
foura1210 <foura3207@gmail.com>
2024-11-10 16:14:39 +0900

    GM-181 refactor: TypeMaterialFile 변환 메서드 수정

--- src/main/java/com/example/gemm_server/dto/common/TypedMaterialFile.java
+++ src/main/java/com/example/gemm_server/dto/common/TypedMaterialFile.java
@@ -1,6 +1,8 @@
 package com.example.gemm_server.dto.common;
 
 import com.example.gemm_server.common.enums.MaterialType;
+import com.example.gemm_server.common.util.MaterialUtil;
+import java.util.List;
 import lombok.Getter;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -13,5 +15,14 @@ public class TypedMaterialFile {
   public TypedMaterialFile(MaterialType type, MultipartFile file) {
     this.type = type;
     this.file = file;
+  }
+
+  public static TypedMaterialFile convertTo(MultipartFile file) {
+    MaterialType materialType = MaterialUtil.getMaterialType(file.getOriginalFilename());
+    return new TypedMaterialFile(materialType, file);
+  }
+
+  public static List<TypedMaterialFile> convertTo(List<MultipartFile> files) {
+    return files.stream().map(TypedMaterialFile::convertTo).toList();
   }
 }
