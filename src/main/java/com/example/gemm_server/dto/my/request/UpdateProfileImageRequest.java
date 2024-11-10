3e6bb6ccdfbe741a4afa3129cbf5127421ea5a60
foura1210 <foura3207@gmail.com>
2024-11-10 16:28:23 +0900

    GM-181 feat: 파일 validator 제거

--- src/main/java/com/example/gemm_server/dto/my/request/UpdateProfileImageRequest.java
+++ src/main/java/com/example/gemm_server/dto/my/request/UpdateProfileImageRequest.java
@@ -1,6 +1,5 @@
 package com.example.gemm_server.dto.my.request;
 
-import com.example.gemm_server.common.annotation.file.ValidFile;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +10,5 @@ import org.springframework.web.multipart.MultipartFile;
 public class UpdateProfileImageRequest {
 
   @NotNull
-  @ValidFile(maxSize = 2097152) // 임시로 2MB
   private MultipartFile profileImage;
 }
