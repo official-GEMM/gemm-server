5b492e517038ac5361cb62c780450634acb89e0a
prefer52 <prefer0502@gmail.com>
2024-11-14 16:31:58 +0900

    GM-192 fix: POI 유틸 및 S3 유틸에 기존 함수 롤백

--- src/main/java/com/example/gemm_server/common/util/PoiUtil.java
+++ src/main/java/com/example/gemm_server/common/util/PoiUtil.java
@@ -18,8 +18,12 @@ import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 import javax.imageio.ImageIO;
@@ -37,6 +41,70 @@ public class PoiUtil {
   private static final String PNG = "png";
   private static final String PDF = "pdf";
   private static final String FONT = "BMJUA_otf.otf";
+
+  public static List<String> convertPptToPng(InputStream file, String fileName) {
+    try (XMLSlideShow ppt = new XMLSlideShow(file)) {
+      List<String> imageFileNames = new ArrayList<>();
+      Dimension pgsize = ppt.getPageSize();
+      List<XSLFSlide> slides = ppt.getSlides();
+      BufferedImage img;
+
+      for (int i = 0; i < slides.size(); i++) {
+        img = new BufferedImage(pgsize.width, pgsize.height, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics = img.createGraphics();
+
+        graphics.setPaint(Color.white);
+        graphics.fill(new Rectangle2D.Float(0, 0, pgsize.width, pgsize.height));
+        graphics.setFont(Font.getFont("BMJUA_otf.otf"));
+
+        slides.get(i).draw(graphics);
+        graphics.dispose();
+        String savedFileName =
+            fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf('.')) + i + '.'
+                + PNG;
+        try (FileOutputStream out = new FileOutputStream(savedFileName)) {
+          ImageIO.write(img, PNG, out);
+          imageFileNames.add(savedFileName);
+        }
+      }
+      return imageFileNames;
+    } catch (IOException e) {
+      throw new GeneratorException(FAILED_TO_GENERATE_PPT_THUMBNAIL);
+    }
+  }
+
+  public static String convertDocxToPdf(InputStream file, String fileName) {
+    try {
+      XWPFDocument document = new XWPFDocument(file);
+      PdfOptions options = PdfOptions.create();
+      options.fontProvider(
+          (familyName, encoding, size, style, color) -> FontFactory.getFont("BMJUA_otf.otf",
+              BaseFont.IDENTITY_H, BaseFont.EMBEDDED, size, style, color));
+
+      String filePath =
+          fileName.substring(fileName.lastIndexOf('/') + 1, fileName.lastIndexOf('.') + 1) + PDF;
+      OutputStream out = new FileOutputStream(new File(filePath));
+      PdfConverter.getInstance().convert(document, out, options);
+      return filePath;
+    } catch (IOException ex) {
+      throw new GeneratorException(FAILED_TO_GENERATE_ACTIVITY_SHEET_THUMBNAIL);
+    }
+  }
+
+  public static String convertPdfToPng(String filePath) {
+    String newFilePath = null;
+    try (PDDocument document = PDDocument.load(new File(filePath));) {
+      BufferedImage bufferedImage = new PDFRenderer(document).renderImage(0);
+
+      newFilePath = filePath.substring(0, filePath.lastIndexOf('.') + 1) + PNG;
+      ImageIO.write(bufferedImage, "PNG", new File(newFilePath));
+    } catch (IOException e) {
+      throw new GeneratorException(FAILED_TO_GENERATE_ACTIVITY_SHEET_THUMBNAIL);
+    } finally {
+      new File(filePath).delete();
+      return newFilePath;
+    }
+  }
 
   public static List<MultipartFile> convertFileToPngs(TypedMaterialFile typedMaterialFile,
       String fileName) {
